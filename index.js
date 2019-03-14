f7ed1d9d8955c99d66e6f14efe08903ca8aed685
msangui <msangui@gmail.com>
2019-03-13 23:16:09 -0300

    initial commit with express and tests

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/hello', function (req, res) {
+  res.status(200).send({
+    message: 'hello',
+  });
+});
+
+app.get('/bye', function (req, res) {
+  res.status(200).send({
+    message: 'bye',
+  });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('RUNNING EXPRESS SERVER IN PORT', process.env.PORT || 3000);
+});
+
+module.exports = app;
---
+++ tests/index.test.js
@@ -0,0 +1,13 @@
+var request = require('supertest');
+var app = require('../index.js');
+
+
+describe('try get methods', () => {
+  it('/hello should respond with the correct json message', (done) => {
+    request(app).get('/hello').expect(JSON.stringify({message: 'hello'}), done);
+  });
+
+  it('/bye should respond with the correct json message', (done) => {
+    request(app).get('/bye').expect(JSON.stringify({message: 'bye'}), done);
+  });
+});
